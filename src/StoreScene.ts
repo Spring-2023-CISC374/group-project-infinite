a08ee4be2dbb4e1037ef8be5a5df59a9b0c4449d
printemps25 <124960699+printemps25@users.noreply.github.com>
2023-04-06 06:02:40 -0400

    Update StoreScene.ts

--- src/StoreScene.ts
+++ src/StoreScene.ts
@@ -6,12 +6,18 @@ export default class StoreScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('store', 'assets/dessertbackground.png');
+        this.load.image('store', 'assets/dessertbackground.png'); 
     }
        
-    create() {
+    create() {     
+        const storeButton = this.add.text(320, 20, 'Store').setFont('25px Arial').setColor('#ffffff').setInteractive();
+
         this.add.image(545, 305, 'store');
-        
+
+        storeButton.on('pointerdown', () => {
+            this.scene.start('StoreScene');
+        }, this);
+  
         const backButton = this.add.text(20, 20, 'Back').setFont('23px Arial').setColor('#ffffff').setInteractive();
   
         backButton.on('pointerdown', () => {
