35292eeb3657dd912f63bd055ece4b912ca7059f
Ryan Evans <ryancev@udel.edu>
2023-04-17 14:58:58 -0400

    Removed commented line from Frosting object

--- src/Objects/Frosting.ts
+++ src/Objects/Frosting.ts
@@ -1,6 +1,4 @@
 export default class Frosting extends Phaser.GameObjects.Image {
-    // const bluefrost = this.add.sprite(900, 320,'blue-frosting').setScale(.15).setInteractive();
-
 
     constructor(scene: Phaser.Scene, xVal: number, yVal: number, frostingKey: string) {
         super(scene, xVal, yVal, frostingKey);
