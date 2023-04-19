04751dd32042ca3c04f2dc46462b4b6390e6a669
Alexander Rusin <arusin@udel.edu>
2023-04-19 19:10:36 -0400

    need to fix overlap between player and stove

--- src/KitchenScene.ts
+++ src/KitchenScene.ts
@@ -1,66 +1,70 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
 export default class KitchenScene extends Phaser.Scene {
-    private player?: Phaser.Physics.Arcade.Sprite;
-	private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
-    private interactKey?: Phaser.Input.Keyboard.Key;
-    private zone?: Phaser.GameObjects.Zone;
-    private promptText?: Phaser.GameObjects.Text;
-    
-    constructor() {
-        super("KitchenScene")  
-    }
+  private player?: Phaser.Physics.Arcade.Sprite;
+  private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+  private interactKey?: Phaser.Input.Keyboard.Key;
+  private zone?: Phaser.GameObjects.Zone;
+  private promptText?: Phaser.GameObjects.Text;
 
-    preload() {
-        this.load.image("kitchen", "assets/kitchen.jpeg");
-        this.load.image("instructions", "assets/instructions.png");
-        this.load.image("player", "assets/baker.png");
-    }   
+  constructor() {
+    super("KitchenScene");
+  }
 
-    create() {
-        this.add.image(545, 305, "kitchen");
-		this.cursors = this.input.keyboard.createCursorKeys();
+  preload() {
+    this.load.image("kitchen", "assets/kitchen.jpeg");
+    this.load.image("instructions", "assets/instructions.png");
+    this.load.image("player", "assets/baker.png");
+  }
 
-        this.player = this.physics.add.sprite(100, 100, "player");
-        this.player.setCollideWorldBounds(true);
-        this.interactKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
-        this.zone = this.add.zone(250, 250, 50, 100);
-        this.physics.add.existing(this.zone, true);
+  create() {
+    this.add.image(545, 305, "kitchen");
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.promptText = this.add.text(400, 510, 'Press E to bake!', {
-        fontFamily: 'Arial',
-        fontSize: '40px',  
-        color: '#000'
-        });
-        this.promptText.setVisible(false);
+    this.player = this.physics.add.sprite(100, 100, "player");
+    this.player.setCollideWorldBounds(true);
+    this.interactKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.E
+    );
+    this.zone = this.add.zone(250, 250, 50, 100);
+    this.physics.add.existing(this.zone, true);
 
-        this.physics.add.overlap(this.player, this.zone, () => {
+    this.promptText = this.add.text(400, 510, "Press E to bake!", {
+      fontFamily: "Arial",
+      fontSize: "40px",
+      color: "#000",
+    });
+    this.promptText.setVisible(false);
+
+    this.physics.add.overlap(
+      this.player,
+      this.zone,
+      () => {
         this.promptText?.setVisible(true);
-        this.input.keyboard.once('keydown', (event: KeyboardEvent) => {
-            if (event.key === 'e') {
-                this.scene.start('GameScene');
-            }
+        this.input.keyboard.once("keydown", (event: KeyboardEvent) => {
+          if (event.key === "e") {
+            this.scene.start("GameScene");
+          }
         });
-        }, undefined, this);
-   
-    }  
+      },
+      undefined,
+      this
+    );
+  }
 
-    update() {
-        if (!this.cursors) return;
-  
-		if (this.cursors.left.isDown) {
-			this.player?.setVelocityX(-320);
-		}
-		else if (this.cursors.right.isDown) {
-			this.player?.setVelocityX(320);
-		}
+  update() {
+    if (!this.cursors) return;
 
-		else {
-			this.player?.setVelocityX(0);
-		}
-
-		if (this.cursors.up.isDown && this.player?.body.touching.down) {
-			this.player.setVelocityY(-330);
-		}
+    if (this.cursors.left.isDown) {
+      this.player?.setVelocityX(-320);
+    } else if (this.cursors.right.isDown) {
+      this.player?.setVelocityX(320);
+    } else {
+      this.player?.setVelocityX(0);
     }
+
+    if (this.cursors.up.isDown && this.player?.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,33 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-import HelloWorldScene from './HelloWorldScene'
-import GameScene from './GameScene'
-import KitchenScene from './KitchenScene'
-import InstructionsScene from './InstructionScene'; 
-import StoreScene from './StoreScene';
-import tilemapScene from './tilemapScene';
+import HelloWorldScene from "./HelloWorldScene";
+import GameScene from "./GameScene";
+import KitchenScene from "./KitchenScene";
+import InstructionsScene from "./InstructionScene";
+import StoreScene from "./StoreScene";
+import tilemapScene from "./tilemapScene";
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	width: 12 * 16,
-	height: 6 * 16,
-	zoom: 4,
-	pixelArt: true,
-	physics: {  
-		default: 'matter',
-		matter: {
-			debug: true
-		}
-	},
-	scene: [tilemapScene, HelloWorldScene, KitchenScene, GameScene, InstructionsScene, StoreScene]
-}
+  type: Phaser.AUTO,
+  parent: "app",
+  width: 12 * 16,
+  height: 6 * 16,
+  pixelArt: true,
+  zoom: 6,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: [
+    tilemapScene,
+    HelloWorldScene,
+    KitchenScene,
+    GameScene,
+    InstructionsScene,
+    StoreScene,
+  ],
+};
 
-export default new Phaser.Game(config)
-
+export default new Phaser.Game(config);
