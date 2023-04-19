aa103c9815a0f22e31029490c0acfba8b42ed4d1
Ryan Evans <ryancev@udel.edu>
2023-04-19 14:43:00 -0400

    Added drop functionality

--- src/Objects/Zone.ts
+++ src/Objects/Zone.ts
@@ -4,5 +4,9 @@ export default class Zone extends Phaser.GameObjects.Zone {
         const height = 70;
         super(scene, x, y, width, height);
         this.setRectangleDropZone(width, height);
+
+        scene.input.on('drop', (_pointer: Phaser.Input.Pointer, gameObject: any, dropZone: Phaser.GameObjects.Zone) => {
+            gameObject.setPosition(dropZone.x, dropZone.y);
+        })
     }
 }
