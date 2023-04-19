4fb6adf51947dd0d699544edbe7cd5d047d770f8
Ryan Evans <ryancev@udel.edu>
2023-04-19 14:42:32 -0400

    Refactored drag and drop code

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -33,52 +33,6 @@ export default class GameScene extends Phaser.Scene {
         const coinImage = this.add.image(20, 20, 'coin').setScale(0.05);
         const coinCounterText = this.add.text(40, 10, `x ${coins}`, { fontSize: '24px', fill: '#000' });
 
-        // pinkfrost.depth = 100
-        // bluefrost.depth = 100  
-        // yellowfrost.depth = 100
-        
-
-        // this.input.on('drag', function (pointer: any, gameObject: { x: any; y: any },dragX: any, dragY: any){
-        //     gameObject.x = dragX;
-        //     gameObject.y = dragY;
-        // })
-        // this.input.on('dragenter', function (pointer: any, gameObject: any, dropZone: any) {
-
-        //     graphics.clear();
-        //     graphics.lineStyle(2, 0x00ffff);
-        //     graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
-    
-        // });
-        // this.input.on('dragleave', function (pointer: any, gameObject: any, dropZone: any) {
-
-        //     graphics.clear();
-        //     graphics.lineStyle(2, 0xffff00);
-        //     graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
-    
-        // });
-        // this.input.on('drop', function (pointer: any, gameObject: { x: any; y: any; input: { enabled: boolean } }, dropZone: { x: any; y: any }) {
-
-        //     gameObject.x = dropZone.x;
-        //     gameObject.y = dropZone.y;
-    
-        //     gameObject.input.enabled = true;
-    
-        // });
-
-        // this.input.on('dragend', function (pointer: any, gameObject: { x: any; input: { dragStartX: any; dragStartY: any }; y: any }, dropped: any) {
-
-        //     if (!dropped)
-        //     {
-        //         gameObject.x = gameObject.input.dragStartX;
-        //         gameObject.y = gameObject.input.dragStartY;
-        //     }
-    
-        //     graphics.clear();
-        //     graphics.lineStyle(2, 0xffff00);
-        //     graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
-    
-        // });
-
         const zone1 = new Zone(this, 400, 350);
         const graphic1 = new DropZoneGraphic(this, 400, 350);
 
@@ -90,22 +44,11 @@ export default class GameScene extends Phaser.Scene {
             coinCounterText.setText(`x ${coins}`);
         }
   
-        // this.input.on('drop', (pointer, gameObject, dropZone) => {
-        //     gameObject.x = dropZone.x;
-        //     gameObject.y = dropZone.y;
-        //     gameObject.input.enabled = true;
-            
-        //     if ((gameObject === pinkfrost || gameObject === bluefrost || gameObject === yellowfrost) && (dropZone === zone || dropZone === zone2)) {
-        //         updateCoinCounter();
-        //     }
-
-
         // const storeButton = this.add.text(960, 20, 'Store').setFont("32px Arial").setColor("#ffffff").setInteractive();
         
         // storeButton.on('pointerdown', () => {
         //     this.scene.start('StoreScene');
         // }, this);
 
-        // });
     }
 }
