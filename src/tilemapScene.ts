50e063c0c6d5965d964f517826668d66c01f43e5
Alexander Rusin <arusin@udel.edu>
2023-04-22 16:00:49 -0400

    fixed tilemap

--- src/tilemapScene.ts
+++ src/tilemapScene.ts
@@ -4,6 +4,7 @@ export default class tilemapScene extends Phaser.Scene {
   private player?: Phaser.Physics.Arcade.Sprite;
   private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
   private stove?: Phaser.Types.Tilemaps.TiledObject;
+  private promptText?: Phaser.GameObjects.Text;
   constructor() {
     super("tilemapScene");
   }
@@ -18,20 +19,22 @@ export default class tilemapScene extends Phaser.Scene {
   }
 
   create() {
-    let collisionDetected = false;
-
-    function handleCollision() {
-      if (!collisionDetected) {
-        console.log("Collision detected!");
-        collisionDetected = true;
-      }
-    }
+    var eKey = this.input.keyboard.addKey("E");
     const map = this.make.tilemap({ key: "map" });
     const tileset = map.addTilesetImage("kitchen", "tiles");
     const floor = map.createLayer("floor", tileset, 0, 0);
     const background = map.createLayer("background", tileset, 0, 0);
     const equipment = map.createLayer("equipment", tileset, 0, 0);
     const stove = map.createLayer("stove", tileset, 0, 0);
+    this.promptText = this.add.text(0, 0, "Press E to bake!", {
+      fontFamily: "Arial",
+      fontSize: "10px",
+      color: "#000",
+    });
+    const handleCollision = () => {
+      this.promptText?.setVisible(true);
+    };
+    this.promptText?.setVisible(false);
 
     console.log(this.stove);
 
@@ -42,16 +45,12 @@ export default class tilemapScene extends Phaser.Scene {
     floor.setCollisionByProperty({ collides: true });
     this.player.setCollideWorldBounds(true);
     this.physics.add.collider(this.player, floor);
-    this.physics.add.collider(this.player, stove, handleCollision, null, this);
-    this.physics.add.collider(
-      this.player,
-      stove,
-      () => {
-        collisionDetected = false;
-      },
-      null,
-      this
-    );
+    stove.setTileIndexCallback(111, handleCollision, this);
+    this.physics.add.overlap(this.player, stove);
+
+    eKey.on("down", () => {
+      this.scene.start("GameScene");
+    });
   }
 
   update() {
