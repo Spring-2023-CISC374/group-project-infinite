63064db32d7051a31062593260432aec004b8888
Ryan Evans <ryancev@udel.edu>
2023-04-25 18:38:30 -0400

    Changed variables to be fields of class

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -6,9 +6,24 @@ import DropZone from './Objects/DropZone'
 
 
 export default class GameScene extends Phaser.Scene {
+
+    linerZone?: DropZone;
+    linerZoneGraphic?: DropZoneGraphic;
+
+    frostingZone?: DropZone;
+    frostingZoneGraphic?: DropZoneGraphic;
+
+    blueFrosting?: Frosting;
+    pinkFrosting?: Frosting;
+    yellowFrosting?: Frosting;
+
+    blueLiner?: Liner;
+    pinkLiner?: Liner;
+
 	constructor() {
 		super("GameScene")
 	}
+
     preload() {
         this.load.image('bakery2','assets/bakery.png')
         this.load.image('blue-liner', 'assets/liner-blue.png')
@@ -22,25 +37,23 @@ export default class GameScene extends Phaser.Scene {
     create() {
         this.add.image(545, 305,'bakery2'); 
 
-        const linerZone = new DropZone(this, 400, 350, 'linerZone');
-        const linerZoneGraphic = new DropZoneGraphic(this, 400, 350);
+        this.linerZone = new DropZone(this, 400, 350, 'linerZone');
+        this.linerZoneGraphic = new DropZoneGraphic(this, 400, 350);
 
-        const frostingZone = new DropZone(this, 400, 275, 'frostingZone');
-        const frostingZoneGraphic = new DropZoneGraphic(this, 400, 275);
+        this.frostingZone = new DropZone(this, 400, 275, 'frostingZone');
+        this.frostingZoneGraphic = new DropZoneGraphic(this, 400, 275);
 
-        const blueFrosting = new Frosting(this, 900, 320, 'blue-frosting', frostingZone);
-        const pinkFrosting = new Frosting(this, 820, 320, 'pink-frosting', frostingZone);
-        const yellowFrosting = new Frosting(this, 740, 320, 'yellow-frosting', frostingZone);
+        this.blueFrosting = new Frosting(this, 900, 320, 'blue-frosting', this.frostingZone);
+        this.pinkFrosting = new Frosting(this, 820, 320, 'pink-frosting', this.frostingZone);
+        this.yellowFrosting = new Frosting(this, 740, 320, 'yellow-frosting', this.frostingZone);
 
-        const pinkLiner = new Liner(this, 150, 570, 'pink-liner', linerZone);
-        const blueLiner = new Liner(this, 250, 570, 'blue-liner', linerZone);
+        this.pinkLiner = new Liner(this, 150, 570, 'pink-liner', this.linerZone);
+        this.blueLiner = new Liner(this, 250, 570, 'blue-liner', this.linerZone);
 
         let coins = 0;
         const coinImage = this.add.image(20, 20, 'coin').setScale(0.05);
         const coinCounterText = this.add.text(40, 10, `x ${coins}`, { fontSize: '24px', fill: '#000' });
 
-
-        
         function updateCoinCounter() {
             coins++;
             coinCounterText.setText(`x ${coins}`);
@@ -51,6 +64,10 @@ export default class GameScene extends Phaser.Scene {
         // storeButton.on('pointerdown', () => {
         //     this.scene.start('StoreScene');
         // }, this);
+
+    }
+
+    update() {
 
     }
 }
