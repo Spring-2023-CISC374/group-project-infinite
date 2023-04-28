d06d44c893d4f5f3556de398da59a4eb8c9000d6
saranoor <saranoor@udel.edu>
2023-04-27 23:26:31 -0400

    finish duplicate cupcakes

--- src/BakeScene.ts
+++ src/BakeScene.ts
@@ -8,13 +8,15 @@ import Cupcake from './Objects/Cupcake';
 export default class BakeScene extends Phaser.Scene {
     protected liner!: Liner
     protected frosting!: Frosting;
+    protected orderCount!: number;
 
 	constructor() {
         super("BakeScene");
 	}
-    init(data:{liner:Liner, frosting:Frosting}){
+    init(data:{liner:Liner, frosting:Frosting, count:number}){
         this.frosting = data.frosting;
         this.liner = data.liner;
+        this.orderCount = data.count;
     }
     preload() {
         this.load.image('bakery2','assets/bakery.png')
@@ -30,17 +32,31 @@ export default class BakeScene extends Phaser.Scene {
 
 
     create() {
-        let count = 0; 
         this.add.image(545, 305,'bakery2'); 
+        let count = 0; 
+        const countText = this.add.text(292, 310, `${count}`).setFontSize(30);
         console.log(this.frosting.key);
         this.add.image(400, 290, this.frosting.key).setScale(.2).setDepth(100);
         this.add.image(400, 350, this.liner.key).setScale(.6);
-        this.add.image(550, 280, 'uparrow').setScale(.025).setInteractive().on('pointerdown', () => this.updateCount(++count));
-        this.add.image(550, 370, 'downarrow').setScale(.025).setInteractive().on('pointerdown', () => this.updateCount(--count));
+        this.add.image(300, 280, 'uparrow').setScale(.025).setInteractive().on('pointerdown', () => updateCount(++count));
+        this.add.image(300, 370, 'downarrow').setScale(.025).setInteractive().on('pointerdown', () => updateCount(--count));
+        const FinishOrder = this.add.text(240, 390, "Finish Order").setFontSize(20).setInteractive().on('pointerdown', () => createCupcakes(count));
 
+        const createCupcakes = (count: number) =>{
+            let xCor = 450;
+            for(let i = 1; i < count; i++ ){
+                this.add.image(xCor, 290, this.frosting.key).setScale(.2).setDepth(100);
+                this.add.image(xCor, 350, this.liner.key).setScale(.6);
+                xCor = xCor+50;
+            }
+            this.time.addEvent({delay: 500});
+            if(this.orderCount == count){
+                this.add.text(300, 120, "ORDER COMPLETED").setFontSize(40);
+            }
+        }
         function updateCount(count: number){
             console.log(count)
-            //this.countText.setText(` hello ${count} `);
+            countText.setText(`${count}`)
         }
     
     }
