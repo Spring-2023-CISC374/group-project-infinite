644d9bd92ebf079690e861a39be1123f098fc1c5
printemps25 <124960699+printemps25@users.noreply.github.com>
2023-05-05 19:38:33 -0400

    Update GameScene.ts

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -23,22 +23,25 @@ export default class GameScene extends Phaser.Scene {
 
   userCupcake: Cupcake | null = null;
 
+  private cupcakeAnimationScene!: CupcakeAnimationScene;
+
   constructor() {
     super("GameScene");
   }
 
   preload() {
-    this.load.image("bakery2", "assets/newbakery_drawing.png");
+    this.load.image("bakery2", "assets/third_bakery.png");
     this.load.image("blue-liner", "assets/liner-blue.png");
     this.load.image("pink-liner", "assets/liner-pink.png");
     this.load.image("pink-frosting", "assets/pink-frosting.png");
     this.load.image("blue-frosting", "assets/blue-frosting.png");
     this.load.image("yellow-frosting", "assets/yellow-frosting.png");
+    this.load.image("delivery-box", "assets/deliverybox3.png");
     this.load.image("coin", "assets/coin.png");
   }
 
   create() {
-    this.add.image(550, 305, "bakery2").setScale(0.665);
+    this.add.image(550, 303, "bakery2").setScale(0.671);
     this.linerZone = new DropZone(this, 400, 350, "linerZone");
     this.linerZoneGraphic = new DropZoneGraphic(this, 400, 350);
 
@@ -47,22 +50,22 @@ export default class GameScene extends Phaser.Scene {
 
     this.blueFrosting = new Frosting(
       this,
-      900,
-      320,
+      540,
+      575,
       "blue-frosting",
       this.frostingZone
     );
     this.pinkFrosting = new Frosting(
       this,
-      820,
-      320,
+      740,
+      575,
       "pink-frosting",
       this.frostingZone
     );
     this.yellowFrosting = new Frosting(
       this,
-      740,
-      320,
+      930,
+      575,
       "yellow-frosting",
       this.frostingZone
     );
@@ -90,20 +93,29 @@ export default class GameScene extends Phaser.Scene {
       fontSize: "14px",
       color: "#000",
     });
-    const orderText2 = this.add.text(77, 195, `Liner: \nFrosting: `, {
+    const orderText2 = this.add.text(78, 195, `Liner: \nFrosting: `, {
       fontSize: "12.5px",
       color: "#000",
     });
 
-    const updateOrder = () => {
-      const liners = ["blue", "pink"];
-      const frostings = ["yellow", "blue", "pink"];
+    const deliveryBox = this.add.sprite(740, 320, "delivery-box").setScale(0.5);;
 
-      const randomLiner = liners[Math.floor(Math.random() * liners.length)];
-      const randomFrosting =
+    const updateOrder = () => {
+      const liners = [this.blueLiner, this.pinkLiner];
+      const frostings = [this.yellowFrosting, this.blueFrosting, this.pinkFrosting];
+      const counts = [1, 2, 4, 6, 8]
+
+      if(this.flag == false){
+        this.count = 1;
+      }else{
+        this.count = counts[Math.floor(Math.random() * counts.length)]
+      }
+
+      this.orderLiner = liners[Math.floor(Math.random() * liners.length)];
+      this.orderFrosting =
         frostings[Math.floor(Math.random() * frostings.length)];
 
-      orderText2.setText(`Liner: ${randomLiner}\nFrosting: ${randomFrosting}`);
+      orderText2.setText(`Liner: ${this.orderLiner?.key.substring(0,this.orderLiner?.key.indexOf('-'))}\nFrosting: ${this.orderFrosting?.key.substring(0,this.orderFrosting?.key.indexOf('-'))}\nCount: ${this.count}`);
     };
 
     updateOrder();
@@ -114,10 +126,17 @@ export default class GameScene extends Phaser.Scene {
     };
 
     const finishCupcake = this.add
-      .text(460, 350, "Bake Cupcake")
-      .setFontSize(20);
-    finishCupcake.setInteractive();
-    finishCupcake.on("pointerdown", () => this.startBakeScene());
+    .text(460, 350, "Bake Cupcake")
+    .setFontSize(20);
+  finishCupcake.setInteractive();
+  finishCupcake.on("pointerdown", () => {
+    if (this.userCupcake != null) {
+      this.userCupcake.printCupcake();
+      this.scene.start("BakeScene", this.getCupcake());
+      // call the create method of the existing instance
+      this.cupcakeAnimationScene.create(this.getCupcake());
+      }
+    });
 
     const storeButton = this.add
       .text(960, 20, "Store")
@@ -137,9 +156,10 @@ export default class GameScene extends Phaser.Scene {
   startBakeScene(): void {
     if (this.userCupcake != null) {
       this.userCupcake.printCupcake();
-      this.scene.start("BakeScene", this.getCupcake());
+      this.scene.start('CupcakeAnimationScene');
     }
   }
+  
 
   updateCupcake(): void {
     if (!this.frostingZone || !this.linerZone) return;
